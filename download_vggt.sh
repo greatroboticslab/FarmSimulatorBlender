1704cef27f2c21f21181bedb2860d295ebf1a513
seanb2022 <sab2ab@mtmail.mtsu.edu>
2025-09-17 12:12:17 -0500

    Updated file structure.

--- download_vggt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-scp -r byler@data.bridges2.psc.edu:"/ocean/projects/cis240145p/byler/HumanoidRobotTrainingData/vggt_baseline/output/" ./vggt_raw/
-scp -r byler@data.bridges2.psc.edu:"/ocean/projects/cis240145p/byler/HumanoidRobotTrainingData/image_processing/vegetablesandfruits/" ./vggt_images/
